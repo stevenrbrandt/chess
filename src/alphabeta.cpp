7455918d8202f0c7231520045865f5b38deb0510
Steven R. Brandt <sbrandt@cct.lsu.edu>
2015-06-15 16:13:04 -0500

    Re-fixing the fix.

--- src/alphabeta.cpp
+++ src/alphabeta.cpp
@@ -244,26 +244,26 @@ score_t search_ab(boost::shared_ptr<search_info> proc_info)
 
     bool store = true;
     score_t lo, hi;
-    if(max_val < alpha) {
+    if(max_val <= alpha) {
       lo = max_val;
       hi = zhi;
     } else if(alpha < max_val && max_val < beta) {
       lo = hi = max_val;
-    } else if(max_val > beta) {
+    } else if(max_val >= beta) {
       hi = max_val;
       lo = zlo;
     } else {
       store = false;
     }
 
-   // if(store) {
+    if(store) {
       if (board.side==LIGHT && board.depth > 3) {
         white = board.side ==LIGHT;
         dbase.add_data(board,lo,hi,white);
       } else {
         set_transposition_value(board,lo,hi);
       }
-   // }
+    }
 
     return max_val;
 }
