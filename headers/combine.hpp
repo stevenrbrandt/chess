5e530edfeeeef511e1d198a173223eab2e3d314e
linamaria11 <lpulgari@andrew.cmu.edu>
2015-07-13 13:43:03 -0500

    merge working

--- headers/combine.hpp
+++ headers/combine.hpp
@@ -17,12 +17,12 @@ const char *sql;
 
 static int callbackFinal(void *NotUsed, int argc, char **argvalue, char **azColName){ 
   std::ostringstream o;
-  std::cout<< argc << std::endl;
-  o<<"INSERT OR REPLACE INTO white (depth,board,hi,lo,hash,sumdepth) VALUES ("<<argvalue[0]<<","<<argvalue[1]<<","<<argvalue[2]<<","<<argvalue[3]<<","<<argvalue[4]<<","<<argvalue[5]<<");"; 
+  o<<"INSERT OR REPLACE INTO white (depth, board, hi, lo, hash, sumdepth) VALUES (\'"<<argvalue[0]<<"\',\'"<<argvalue[1]<<"\',\'"<<argvalue[2]<<"\',\'"<<argvalue[3]<<"\',\'"<<argvalue[4]<<"\',\'"<<argvalue[5]<<"\');"; 
   //updating db from db2 if it has better values at 
   std::string update = o.str();
   sql = update.c_str();
-  rc = sqlite3_exec(db,sql,0,0,&zErrMsg);
+  if (argvalue[0]!= NULL)
+    rc = sqlite3_exec(db,sql,0,0,&zErrMsg);
   if (rc != SQLITE_OK){
     fprintf(stderr, "SQL error callbackFinal: %s\n", zErrMsg);
     sqlite3_free(zErrMsg);
@@ -33,33 +33,42 @@ static int callbackFinal(void *NotUsed, int argc, char **argvalue, char **azColN
 static int callback2(void *Used, int argc, char **argvalue, char **azColName);
 static int callback(void *NotUsed, int argc, char **argvalue, char **azColName){
   std::ostringstream select2;
-  select2<<"SELECT * FROM white WHERE BOARD="<<argvalue[1]<<" AND"<<"DEPTH ="<<argvalue[0]<<";";
+  select2<<"SELECT * FROM white WHERE DEPTH=\'"<<argvalue[0]<<"\' AND"<<"BOARD =\'"<<argvalue[1]<<"\';";
   std::string merge = select2.str();
   sql = merge.c_str();
   compare params;
   rc = sqlite3_exec(db, sql, callback2,&params,&zErrMsg);
   std::ostringstream o;
-  o<<"UPDATE white SET"<<azColName[2]<<"="<<argvalue[2]<<", "<<azColName[3]<<"="<<argvalue[3]<<", "<<azColName[4]<<"="<<argvalue[4]<<", "<<azColName[5]<<"="<<argvalue[5]<<" WHERE BOARD ="<<argvalue[1]<<" AND DEPTH ="<<argvalue[0]<<";"; 
+  o<<"UPDATE white SET \'"<<azColName[2]<<"\'=\'"<<argvalue[2]<<"\', \'"<<azColName[3]<<"\'=\'"<<argvalue[3]<<"\', \'"<<azColName[4]<<"\'=\'"<<argvalue[4]<<"\', \'"<<azColName[5]<<"\'=\'"<<argvalue[5]<<"\' WHERE BOARD =\'"<<argvalue[1]<<"\' AND DEPTH =\'"<<argvalue[0]<<"\';"; 
   //updating db from db2 if it has better values at 
   std::string update = o.str();
   sql = update.c_str();
   int compare = atoi(argvalue[3]);
-  if (params.size() == 0)
+  std::ostringstream del;
+  del<<"DELETE FROM white WHERE DEPTH="<<argvalue[0]<<" AND BOARD="<<argvalue[1]<<";";
+  std::string db_delete = del.str();
+  const char *sql_del = db_delete.c_str();
+  if (params.size() < 5){
     return 0;
+    }
   if (params.size() == 5){
     if (compare < atoi(params.at(3).c_str())){
-      rc = sqlite3_exec(db,sql,0,0,0);
+      rc = sqlite3_exec(db,sql,0,0,&zErrMsg);
+      rc = sqlite3_exec(db2, sql_del,0,0,0); 
+      assert(false);
       //then delete value from db2
     }
     if (compare>=atoi(params.at(3).c_str())){
       //delete value from db2
+      rc = sqlite3_exec(db2, sql_del,0,0,0);
+      assert(false);
     }
   }
-  rc = sqlite3_exec(db, sql, 0,0,0);
   if (rc != SQLITE_OK){
     fprintf(stderr, "SQL error callback: %s\n", zErrMsg);
     sqlite3_free(zErrMsg);
     }else{
+      fprintf(stderr, "Replaced better values successfully");
     }
   return 0;
 }
@@ -69,9 +78,8 @@ static int callback2(void *Used, int argc, char **argvalue, char **azColName){
   for(int i=0;i<argc;i++) {
     params->push_back(argvalue[i]);
   }
-  return 0;
   std::ostringstream del;
-  del<<"DELETE FROM white WHERE BOARD="<<argvalue[1]<<" AND DEPTH ="<<argvalue[0]<<"AND LO<"<<argvalue[3]<<";";
+  del<<"DELETE FROM white WHERE DEPTH="<<argvalue[0]<<" AND BOARD="<<argvalue[1]<<";";
   std::string db_delete = del.str();
   sql = db_delete.c_str();
   rc = sqlite3_exec(db2, sql, 0,0,0);
@@ -79,7 +87,9 @@ static int callback2(void *Used, int argc, char **argvalue, char **azColName){
     fprintf(stderr, "SQL error callback: %s\n", zErrMsg);
     sqlite3_free(zErrMsg);
     }else{
+      fprintf(stderr, "Callback 2 successful");
     }
+  return 0;
 }
 
 static int callback(void *NotUsed, int argc, char **argvalue, char **azColName);
@@ -87,7 +97,19 @@ static int callbackFinal(void *NotUsed, int argc, char **argvalue, char **azColN
 
 void merge(std::vector<std::string> databases)
 {
+  sqlite3_close(db);
   std::string current_database = databases.at(0).c_str();
+  std::ostringstream l;
+  l<<current_database<<".db";
+  std::string open_1 = l.str();
+  sql = open_1.c_str();
+  rc = sqlite3_open(sql, &db);
+    if(rc){
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      exit(0);
+    }else{
+      fprintf(stderr, "Opened test1 database successfully\n");
+      }
   for(int i(1); i<databases.size(); ++i)
   {
     std::string merging_database = databases.at(i).c_str();
@@ -95,17 +117,15 @@ void merge(std::vector<std::string> databases)
     o<<merging_database<<".db";
     std::string open = o.str();
     sql = open.c_str();
-
     rc = sqlite3_open(sql, &db2);
     if(rc){
       fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
       exit(0);
     }else{
-      fprintf(stderr, "Opened database successfully\n");
+      fprintf(stderr, "Opened database testx successfully\n");
       }
 
-    rc = sqlite3_exec(db, "PRAGMA journal_mode=WAL;", 0, 0, &zErrMsg);  
-    rc = sqlite3_exec(db2,"BEGIN;", 0, 0, &zErrMsg);
+    //rc = sqlite3_exec(db2,"BEGIN;", 0, 0, &zErrMsg);
     rc = sqlite3_exec(db2, "SELECT * FROM white;",callback,0,&zErrMsg);
     if (rc != SQLITE_OK){
       fprintf(stderr, "SQL error 1st select: %s\n", zErrMsg);
